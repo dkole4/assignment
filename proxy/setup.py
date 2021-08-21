b146b4570bd7d0dadce037fbabfa6b7fa031e936
Denis Kole <denis.kole@tuni.fi>
2021-08-21 09:48:51 +0300

    Change proxy directory structure

---
+++ proxy/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+   name='server',
+   version='0.1',
+   description='A proxy for assignment app',
+   author='Denis Kole',
+   packages=['server'],
+   install_requires=[]
+)
